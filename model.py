19c6fa7892b376f43f94e1d8b3419a986c648bb2
Emiel Witting <46195853+EWitting@users.noreply.github.com>
2019-01-23 19:25:07 +0100

    .pyc bestanden verwijderd en keras dimensies gefixt(hopelijk)
    het .pyc bestand stond in .gitignore maar werd nog steeds gedetecteerd, dat is hopeijl straks over. De dimensies in keras werken nu als het goed is en er zijn een aantal print statements toegevoegd

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import Adam
+from keras import backend as K
+K.image_dim_ordering
 
 from collections import deque
 import numpy as np
@@ -11,7 +13,7 @@ import simulatie
 
 class agent: #agent is een ander woord voor "een AI" in machine learning
     
-    def __init__(self, max_uren):
+    def __init__(self, max_uren,prints):
         self.model = self.createModel() #start functie om model te maken en bewaar het in self.model
         self.memory = deque(maxlen=1000) #verzameling van experiences, wordt automatisch op maximum lengte gehouden om te voorkomen dat te oude experiences worden gebruikt
         
@@ -20,8 +22,10 @@ class agent: #agent is een ander woord voor "een AI" in machine learning
         self.epsilon_verval = 0.999 #hoe snel epsilon kleiner wordt
         
         self.max_uren = max_uren #bepaald aantal uren dat mag worden uitgekozen om op te leren
+        self.prints = prints
         
     def createModel(self):
+        if self.prints: print('Model initiatie...')
         
         network = Sequential()  #functie van keras om model te maken
         
@@ -38,20 +42,22 @@ class agent: #agent is een ander woord voor "een AI" in machine learning
         #dropout voorkomt overfitting door met een bepaalde kans neuronen te laten negeren, dit voorkomt dat alles maar op één manier werkt
         network.add(Dropout(0.05))
         
-        #één output in de laatste laag, namelijk het verwachtte cijfer.
-        network.add(Dense(1,activation = 'linear'))
+        #één output in de laatste laag, namelijk het verwachtte cijfer. softplus maakt alle getallen positief(de negatieve getallen zullen wel altijd kleiner blijven) om beter mee te kunnen werken
+        network.add(Dense(1,activation = 'softplus'))
         
         #parameters voor keras instellen, dit zijn de gebruikelijkste standaard parameters
         network.compile(loss = 'mse', optimizer = Adam(lr=0.001))
         
+        if self.prints: print('Model is gecompiled')
+        
         return network
         
     def remember(self,situatie,cijfer): # voeg situatie en cijfer toe aan experiences
-        self.memory.add((situatie,cijfer))
+        self.memory.append((situatie,cijfer))
         
     def choose(self, voorspellingen, schema):# kiest uren waarop moet worden geleerd aan de hand van de voorspellingen 
-        leermomenten = [] #index in uren wanneer moet worden geleerd
-        leermomenten.add(heapq.nlargest(self.max_uren, range(len(voorspellingen)), key=voorspellingen.__getitem__)) #gebruikt plugin om de <max_uren> hoogste voorspellingen te vinden
+        #index in uren wanneer moet worden geleerd
+        leermomenten = heapq.nlargest(self.max_uren, range(len(voorspellingen)), key=voorspellingen.__getitem__) #gebruikt plugin om de <max_uren> hoogste voorspellingen te vinden
         
         leeruren = [False]*len(schema) #python array met booleans die false zijn
         
@@ -62,6 +68,7 @@ class agent: #agent is een ander woord voor "een AI" in machine learning
         
     def voorspel(self, schema, moeilijkheidsgraad, onthoud):
         sim = simulatie.Simulatie(moeilijkheidsgraad)
+        if self.prints: print('Simulatie geladen, beginnen met voorspellen...')
         voorspellingen = []
         situaties = []
         for uur in range(len(schema)):
@@ -69,19 +76,29 @@ class agent: #agent is een ander woord voor "een AI" in machine learning
                 tijd = uur % 24 # tijd van de dag in uren
                 tijdTotToets = len(schema) - uur #aantal uren tot de toets is
                 invoer = np.array([tijd,tijdTotToets,moeilijkheidsgraad]) #zet invoer om in numpy array, een formaat dat door keras wordt gebruikt
-                situaties.add(invoer)
-                voorspellingen.add(self.model.predict(invoer)[0])
+                invoer = invoer.reshape(1,3)
+                situaties.append(invoer)             
+                voorspellingen.append((self.model.predict(invoer))[0][0])
             else:
-                situaties.add(0)
-                voorspellingen.add(0)
+                situaties.append(0)
+                voorspellingen.append(0)
         
+        if self.prints: print('voorspellingen gemaakt:',voorspellingen)
+
         leeruren = self.choose(voorspellingen,schema)
+        
+        if self.prints: print('leeruren gekozen:',leeruren)
+        
         cijfer = sim.simuleer(leeruren)
         
+        if self.prints: print('cijfer berekend:',cijfer)
+        
         if onthoud: #als parameter onthoud True is worden de uren waarop is geleerd toegevoegd aan het geheugen/de experiences
-            for uur in range in leeruren:
+            for uur in range(len(leeruren)):
                 if leeruren[uur]:                    
                     self.remember(situaties[uur],cijfer)
+            if self.prints: print('experiences toegevoegd aan geheugen')
+        
         
         return cijfer
     
@@ -92,16 +109,15 @@ class agent: #agent is een ander woord voor "een AI" in machine learning
         
         groep = random.sample(self.memory, groepsgrootte)
         
-        invoer = np.empty()
-        uitkomsten = np.empty()
+        invoer = np.empty((groepsgrootte,3)) 
+        uitkomsten = np.empty((groepsgrootte,1))
         
-        for situatie, cijfer in groep:
-            np.append(invoer, situatie)
-            np.append(uitkomsten, cijfer)
-        
+        for i in range(len(groep)):
+            invoer[i] = groep[i][0]
+            uitkomsten[i] = groep[i][1]
+
+
         self.model.fit(invoer,uitkomsten,epochs=1,verbose=1)
         
         if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_verval
-            
-        
+            self.epsilon *= self.epsilon_verval          
